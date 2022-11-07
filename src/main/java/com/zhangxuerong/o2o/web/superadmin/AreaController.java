a709e2b1b1326bd4c5afc2a89493aeac2ca3b2bf
zhangxuerong <1027092674@qq.com>
2022-11-07 10:27:14 +0800

    update: logback

--- src/main/java/com/zhangxuerong/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/zhangxuerong/o2o/web/superadmin/AreaController.java
@@ -2,6 +2,9 @@ package com.zhangxuerong.o2o.web.superadmin;
 
 import com.zhangxuerong.o2o.entity.Area;
 import com.zhangxuerong.o2o.service.AreaService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +18,7 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+    Logger logger = LoggerFactory.getLogger(AreaController.class);
     private final AreaService areaService;
 
     public AreaController(AreaService areaService) {
@@ -24,6 +28,8 @@ public class AreaController {
     @RequestMapping(value = "/listarea",method = RequestMethod.GET)
     @ResponseBody
     public Map<String,Object> listArea(){
+        logger.info("===start===");
+        long startTime = System.currentTimeMillis();
         Map<String,Object> modelMap = new HashMap<String,Object>();
         List<Area> list =new ArrayList<Area>();
         try {
@@ -35,6 +41,10 @@ public class AreaController {
             modelMap.put("success",false);
             modelMap.put("errMsg",e.toString());
         }
+        long endTime = System.currentTimeMillis();
+        logger.info("===end===");
+        logger.debug("costTime:[{}ms]",endTime - startTime);
+        logger.error("test error!");
         return modelMap;
     }
 }
